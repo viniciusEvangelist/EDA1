4bfd4397abea4c7e3bb21ebff19d5e7d74da08ce
Vinicius Evangelista Borges Pinto <viniciusy222@gmail.com>
2026-06-08 13:12:16 -0300

    exercicio 10

--- lista1EDA.c
+++ lista1EDA.c
@@ -131,4 +131,19 @@ int main(){
     }
 }
 //exercicio 10
-
+void intercala(int *v1, int *v2, int *v3, int n){
+    while (n--){
+        *v3++ = *v1++;
+        *v3++ = *v2++;
+    }
+}
+int main(){
+    int v1[] = {1, 3, 5};
+    int v2[] = {2, 4, 6};
+    int v3[6];
+    intercala(v1, v2, v3, 3);
+    for(int i = 0; i<6; i++){
+        printf("%d ", v3[i]);
+    }
+    return 0;
+}
